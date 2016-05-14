bae8530bfbb03dbf09a0c90cda0be037e0ffb446
Luis Alberto <lalberto8085@gmail.com>
2016-05-13 22:12:42 -0400

    adding console project
    installing Microsoft.Exchange.WebServices library

---
+++ EWSClient/ExchangeClient.fs
@@ -0,0 +1,30 @@
+﻿
+module ExchangeClient
+
+open System
+open System.Net
+open Microsoft.Exchange.WebServices.Data
+
+type Client(endpoint, user, pwd) =
+
+    let properties = 
+        new PropertySet(ItemSchema.Id,
+                        ItemSchema.Subject,
+                        ItemSchema.Categories,
+                        AppointmentSchema.Start,
+                        AppointmentSchema.End,
+                        AppointmentSchema.Location,
+                        AppointmentSchema.IsAllDayEvent,
+                        AppointmentSchema.IsRecurring)
+
+    let getFolderId email = new FolderId(WellKnownFolderName.Calendar, new Mailbox(email))
+
+    let getClient timeZone =
+        let client = ExchangeService(ExchangeVersion.Exchange2010_SP2, timeZone)
+        client.Url <- endpoint
+        client.Credentials <- ExchangeCredentials.op_Implicit(new NetworkCredential())
+
+        34
+
+    member this.GetAppointments(email) =
+        35
