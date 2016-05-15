fe27708586a35c58104ad6f4803787e45b7ea484
Luis Alberto <lalberto8085@gmail.com>
2016-05-15 13:43:50 -0400

    abstracting the EWS appointment

--- EWSClient/ExchangeClient.fs
+++ EWSClient/ExchangeClient.fs
@@ -5,6 +5,20 @@ open System
 open System.Net
 open Microsoft.Exchange.WebServices.Data
 
+type AppointmentDto = {
+    EwsId: string
+    OneId: string option
+    Subject: string option
+    Start: DateTime
+    End: DateTime
+    Categories: string list
+    IsAllDay: bool
+    IsRecurring: bool
+    Location: string
+    AppointmentType: string option
+    Body: string option
+}
+
 let public OneBodyId = new ExtendedPropertyDefinition(DefaultExtendedPropertySet.PublicStrings,
                                                       "AppointmentID", MapiPropertyType.String)
 
@@ -25,7 +39,36 @@ type Client(endpoint, user, pwd) =
                         OneBodyId,
                         AppointmentType)
 
-    let getFolderId email = new FolderId(WellKnownFolderName.Calendar, new Mailbox(email))
+    let loadBody (app: Appointment) =
+        let extendedProperties = new PropertySet(properties)
+        extendedProperties.Add(ItemSchema.Body)
+        app.Load(extendedProperties)
+        app
+
+    let loadExtProp (property: ExtendedPropertyDefinition) (app: Appointment) =
+        let mutable result = ""
+        if app.TryGetProperty(property, ref result) then
+            Some result
+        else
+            None
+
+    let toDto includeBody app =
+        let loaded = if includeBody 
+                     then loadBody app
+                     else app
+        {
+            EwsId = loaded.Id.UniqueId
+            OneId = loadExtProp OneBodyId loaded
+            Subject = loaded.Subject |> Option.ofObj
+            Start = loaded.Start
+            End = loaded.End
+            Categories = loaded.Categories |>  Seq.toList
+            IsAllDay = loaded.IsAllDayEvent
+            IsRecurring = loaded.IsRecurring
+            Location = loaded.Location
+            AppointmentType = loadExtProp OneBodyId loaded
+            Body = if includeBody then loaded.Body.Text |> Option.ofObj else None
+        }
 
     let getClient timeZone =
         let client = ExchangeService(ExchangeVersion.Exchange2010_SP2, timeZone)
@@ -34,7 +77,9 @@ type Client(endpoint, user, pwd) =
 
         client
 
-    member this.GetAppointments(email, startDate, endDate, timeZone) =
+    let getFolderId email = new FolderId(WellKnownFolderName.Calendar, new Mailbox(email))
+
+    let getInInterval email startDate endDate timeZone =
         let client = getClient timeZone
         
         let view = new CalendarView(startDate, endDate)
@@ -42,13 +87,18 @@ type Client(endpoint, user, pwd) =
 
         let folder = getFolderId email
 
-        client.FindAppointments(folder, view)
+        client.FindAppointments(folder, view) 
+
+
+    member this.GetAppointments(email, startDate, endDate, timeZone) =
+        getInInterval email startDate endDate timeZone |> Seq.map (toDto false)
 
 
     member this.GetAppointmentsWithOneBodyId(email, startDate, endDate, timeZone) =
         let mutable obj = ""
-        this.GetAppointments(email, startDate, endDate, timeZone)
+        getInInterval email startDate endDate timeZone
                 |> Seq.filter (fun a -> a.TryGetProperty(OneBodyId, ref obj))
+                |> Seq.map (toDto false)
 
 
     member this.GetByOneId(email, oneId: string, timeZone) =
@@ -59,16 +109,10 @@ type Client(endpoint, user, pwd) =
 
         let client = getClient timeZone
 
-        let loadBody (app: Appointment) =
-            let extendedProperties = new PropertySet(properties)
-            extendedProperties.Add(ItemSchema.Body)
-            app.Load(extendedProperties)
-            app
-
         client.FindItems(folder, filter, view) 
         |> Seq.cast<Appointment> 
         |> Seq.tryHead
-        |> Option.map loadBody
+        |> Option.map (toDto true)
 
 
     member this.GetByOneIds(email, timeZone, oneIds: string list) =
@@ -82,5 +126,7 @@ type Client(endpoint, user, pwd) =
 
         let client = getClient timeZone
 
-        client.FindItems(folder, filter, view) |> Seq.cast<Appointment> |> Seq.toList
+        client.FindItems(folder, filter, view) 
+        |> Seq.cast<Appointment> 
+        |> Seq.map (toDto false)
         
