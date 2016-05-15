cda03693000af553c45391a8ec42e91e9297de23
Luis Alberto <lalberto8085@gmail.com>
2016-05-15 15:32:22 -0400

    adding configuration parser

---
+++ EWSClient/ConfigParser.fs
@@ -0,0 +1,29 @@
+﻿
+module ConfigParser
+
+open System.IO
+open Newtonsoft.Json
+open CommandParsing
+
+type ConfigurationInfo = {
+    Url: string
+    User: string
+    Password: string
+}
+
+type ConfigurationCommand = {
+    Config: ConfigurationInfo
+    Output : OutputType
+    Command: ClientOptions
+}
+
+let private parseConfig (file: string) =
+    
+    use reader = new StreamReader(file)
+    let data = reader.ReadToEnd()
+
+    JsonConvert.DeserializeObject<ConfigurationInfo>(data)
+
+let parseConfiguration (command: CommandOptions) =
+    let config = parseConfig command.ConfigFilePath
+    {Config = config; Output = command.Output; Command= command.Command}
